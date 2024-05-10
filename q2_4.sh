c5060143fb1e2c0688d1d7d3db22b51bae07c242
GavinReddy <gavin_reddy@hotmail.com>
2024-05-10 17:40:56 +0000

    Added a function to create a salary table. However, salary value is not increasing by year in the table sent to output

--- q2_4.sh
+++ q2_4.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
+
+# Generate Salary function
+# Accepts salary, salary points, increment, and managerial status as input
+# Outputs a salary scale for an emplotyee
+
+generate_salary_table(){
+	local starting_salary=$1
+	local num_points=$2
+	local increment=$3
+	local is_managger=$4
+	local this_year=$(date +%Y)
+
+	echo "Salary Scale Table for $this_year:"
+	echo "=================================="
+
+	echo "Year | Point | Salary"
+	echo "-----+-------+-------"
+	
+
+	for ((i = 1; i <= num_points; i++)); do
+		
+		echo "$this_year	| $i	| $salary"
+			
+
+		if [ "$is_manager" = "y" ]; then
+		    salary=$((starting_salary + i * increment))
+		    i = $((  i + 1 ))
+		    echo "$this_year | $i   | $salary"
+		fi    
+	
+		
+		salary=$((starting_salary + i * increment))
+		this_year=$((this_year + 1 ))
+
+	done
+}
+
 # Main Program
 
 echo "Salary Scale Table Generator"
@@ -21,4 +58,7 @@ if [ "$is_manager" != "y" ] && [ "$is_manager" != "n" ]; then
 	echo "Invalid option. Enter 'y' or 'n' for manager status."
 	exit 1
 fi
+
+# Call the functio to generate the salary table
+generate_salary_table "$starting_salary" "$num_points" "$is_manager"
 
