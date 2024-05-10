001ce490a6d9fe55b42a7bf5447e0d8fb7042226
GavinReddy <gavin_reddy@hotmail.com>
2024-05-10 15:33:38 +0000

    Created and tested request to the user to input information at the prompt

--- q2_4.sh
+++ q2_4.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
+# Main Program
+
 echo "Salary Scale Table Generator"
+
+# Request user to input starting salary
+read -p "Enter the starting salary: " starting_salary
+
+# Requet user to input number of points on the salary scale
+read -p "Enter the number of points on the salary scale: " num_points
+
+# Request user to enter the salary increment
+read -p "Enter the salary increment: " increment
+
+# Request user to specify if the employess is a manager
+read -p "Is the employee a manager? (y/n)" is_manager
+
+# check input is working
+
+echo $starting_salary
+echo $num_points
+echo $increment
+echo $is_manager
+
+
