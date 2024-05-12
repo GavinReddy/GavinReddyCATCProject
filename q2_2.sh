6c0da2ea78755f19ac3c9f07aac081029d877b97
GavinReddy <gavin_reddy@hotmail.com>
2024-05-12 22:26:24 +0000

    Simple output of command results. History file not sending output

--- q2_2.sh
+++ q2_2.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
+echo "Command Output Begins"
 echo
+echo "date command"
 date
 echo
+echo "hostname command"
 hostname
 echo
+echo "arch command"
 arch
 echo
+echo "uname -a command"
 uname -a
 echo
+echo "uptime command"
 uptime
 echo
+echo "whoami command"
 whoami
 echo
+echo "who command"
 who
 echo
+echo "last command"
 last
 echo
+echo "w command"
 w
 echo
+echo "history command"
+# Need to read the history file
 history
 echo
+echo "Command Output Ends"
 
